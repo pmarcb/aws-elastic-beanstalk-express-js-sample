d42fe87e8fb45d116bd761da1164dcb361de643f
pmarcb <pmarcb@clt-aws.opti-investment.int>
2021-08-07 15:58:24 +0200

    Full pipeline test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All Times !), two time '));
+app.get('/', (req, res) => res.send('Hello All Times !), three time '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
